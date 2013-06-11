1484a88de03f26248a0a4da8ab90b0bbeab074e8
slava-pampam <slava-osadchiy@mail.ru>
2013-06-12 01:02:04 +0300

    fixed

---
+++ Simple/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: slava
+ *
+ * Created on 12 Июнь 2013 г., 0:41
+ */
+
+#include <cstdlib>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 2;
+}
+
